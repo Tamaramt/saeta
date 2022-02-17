7cb0a743244b07f1b933b78eb8b5083c3831d5c5
Tamaramt <99643663+Tamaramt@users.noreply.github.com>
2022-02-17 19:13:32 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -41,6 +41,10 @@ music.playTone(523, music.beat(BeatFraction.Quarter))
 music.playTone(494, music.beat(BeatFraction.Whole))
 music.playTone(440, music.beat(BeatFraction.Whole))
 music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(494, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Whole))
 basic.forever(function () {
 	
 })
